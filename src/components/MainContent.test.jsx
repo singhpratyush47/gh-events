8c43595aa3ce75f715c97806a3cc2de2afc880d0
singhpratyush47 <singhpratyush47@gmail.com>
2025-05-18 11:52:08 +0530

    added comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -11,6 +11,7 @@ describe('MainContent', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
+  // added comment
   it('should show the help area after clicking the button', async () => {
     render(<MainContent />);
 
